722cc0e7b7f318c2c074b19e736f06f146ef3f40
khalid <khalid.eq85@gmail.com>
2020-09-19 12:23:50 +0300

    new cars

--- pages/views.py
+++ pages/views.py
@@ -5,13 +5,18 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 # Create your views here.
 
 
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('created_date').filter(is_featured=True)
+    all_cars = Car.objects.order_by('-created_date')
     data = {
         'teams': teams,
+        'featured_cars': featured_cars,
+        'all_cars' : all_cars ,
     }
 
     return render(request, 'pages/home.html', data)
@@ -19,6 +24,7 @@ def home(request):
 
 def about(request):
     teams = Team.objects.all()
+
     data = {
         'teams': teams,
     }
