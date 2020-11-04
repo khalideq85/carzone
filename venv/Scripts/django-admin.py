d0c1a9aa2ebaf9c156c1134ae211010d5b9281c8
khalid <khalid.eq85@gmail.com>
2020-11-04 19:29:58 +0300

    corrected runtime

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!d:\carzone\carzone-pycharm\venv\scripts\python.exe
+#!D:\carzone\carzone-pycharm\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
